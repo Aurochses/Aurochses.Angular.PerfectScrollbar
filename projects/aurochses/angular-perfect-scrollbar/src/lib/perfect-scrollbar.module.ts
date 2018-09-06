859c235017ccfb79b5e03b5a6cd84f2ae9e40417
Vladimir Rodchenko <vladimir@rodchenko.by>
2018-09-06 17:36:07 +0300

    directive

---
+++ projects/aurochses/angular-perfect-scrollbar/src/lib/perfect-scrollbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { PerfectScrollbarDirective } from './perfect-scrollbar.directive';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    PerfectScrollbarDirective
+  ],
+  exports: [
+    PerfectScrollbarDirective
+  ]
+})
+export class PerfectScrollbarModule { }
--- projects/aurochses/angular-perfect-scrollbar/src/public_api.ts
+++ projects/aurochses/angular-perfect-scrollbar/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of angular-perfect-scrollbar
  */
 
-export * from './lib/angular-perfect-scrollbar.service';
-export * from './lib/angular-perfect-scrollbar.component';
-export * from './lib/angular-perfect-scrollbar.module';
+export * from './lib/perfect-scrollbar.module';
